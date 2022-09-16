b12497df7612a4511eff78698ec7abcabf8727e6
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-16 19:17:07 +0900

    Time: 60 ms (99.02%), Space: 43.2 MB (98.80%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.js
+++ 190-reverse-bits/190-reverse-bits.js
@@ -2,14 +2,12 @@ function reverseBits(n) {
   let result = 0;
 
   for (let i = 0; i < 32; i++) {
-    // find the last bit of n
-    const lastBit = n & 1;
+    const lastBit = n & 1; // 맨 끝 비트
 
-    // shift the last bit of n to the left
+    // 맨끝으로 비트를 옳김
     const reversedLastBit = lastBit << (31 - i);
-
     // insert the reversed last bit of n into the result
-    result |= reversedLastBit;
+    result += reversedLastBit;
 
     // the last bit of n is already taken care of, so we need to drop it
     n >>>= 1;
