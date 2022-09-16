6f964325674dd99f12dd545ab485467bdf692996
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-16 19:50:18 +0900

    Time: 103 ms (16.92%), Space: 42 MB (33.51%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -0,0 +1,9 @@
+var getSum = function(a, b) {
+      let carry;
+      while((a & b) !== 0){
+          carry = (a & b) << 1;
+          a = a ^ b;
+          b = carry;
+      }
+      return a ^ b;
+};
