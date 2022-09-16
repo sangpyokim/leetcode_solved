f663368e2f7e374a4e9290d0fa936a0b0e071483
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-16 19:03:18 +0900

    Time: 150 ms (45.33%), Space: 48.4 MB (75.95%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -3,17 +3,20 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    const answer = []
-    answer.push(0)
+    if (n === 0) return [0]
+    const answer = Array.from({length: n}, () => 0)
+    
+    let offset = 1
     for (let i = 1; i <= n; i++) {
-        let count = 0
-        let temp = i
-        while(temp !== 0) {
-            count += temp&1
-            temp = temp >>> 1
-            
-        }
-        answer.push(count)
+        if (i === offset * 2) offset = i
+        
+        answer[i] = 1 + answer[i - offset]
     }
     return answer
 };
+
+// 0 1 10 11 100 101 110 111
+// 0 1. 2. 3. 4.  5.  6.  7
+// 1 1. 2. 2. 4.  4.  4.  4
+// 0 1  2. 2
+// 0 1. 1. 2. 1.  2.  2.  3
