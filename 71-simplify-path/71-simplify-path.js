13e45a6fa3b48404b3a0564b746343f7bf4995fc
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-17 19:09:55 +0900

    Time: 100 ms (56.16%), Space: 44.2 MB (53.33%) - LeetHub

--- 71-simplify-path/71-simplify-path.js
+++ 71-simplify-path/71-simplify-path.js
@@ -1,8 +1,8 @@
 var simplifyPath = function(path) {
     let stack = [];
     path = path.split('/');
-    
-    for (let i=0;i<path.length;i++) {
+
+    for (let i in path) {
         if (path[i]=='.' || path[i]=='') continue;
         if (path[i]=='..') stack.pop();
         else stack.push(path[i]);
