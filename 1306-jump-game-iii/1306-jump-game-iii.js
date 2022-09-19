8fec389b153578c984e5441ddfb9b74f188b3120
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-19 16:32:51 +0900

    Time: 148 ms (12.64%), Space: 51.3 MB (48.28%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.js
+++ 1306-jump-game-iii/1306-jump-game-iii.js
@@ -9,17 +9,15 @@ var canReach = function(arr, start) {
     const len = arr.length
     const visited = new Array(len).fill(false)
     function dfs(index) {
-        if (index < 0 || index >= len) return
-        if (visited[index]) return
+        if (index < 0 || index >= len || visited[index]) return
         visited[index] = true
         
         const cur = arr[index]
         
         if (cur === 0) return answer = true
-        else {
-            dfs(index+cur)
-            dfs(index-cur)
-        }
+
+        dfs(index+cur)
+        dfs(index-cur)
     }
     dfs(start)
     
