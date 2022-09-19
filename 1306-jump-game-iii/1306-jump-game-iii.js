7343657583301fda1340dac27ed160e34cff6dd6
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-19 16:27:20 +0900

    Time: 124 ms (40.23%), Space: 51 MB (52.87%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @param {number} start
+ * @return {boolean}
+ */
+var canReach = function(arr, start) {
+    let answer = false
+
+    const len = arr.length
+    const visited = new Array(len).fill(false)
+    function dfs(index) {
+        if (index < 0 || index >= len) return
+        if (visited[index]) return
+        visited[index] = true
+        
+        const cur = arr[index]
+        
+        if (cur === 0) return answer = true
+        else {
+            dfs(index+cur)
+            dfs(index-cur)
+        }
+    }
+    dfs(start)
+    
+    return answer
+};
