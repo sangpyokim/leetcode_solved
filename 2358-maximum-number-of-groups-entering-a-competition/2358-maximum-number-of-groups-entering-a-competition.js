44be508c92f04505d171f8c7c947248a9c264873
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-22 09:19:06 +0900

    Time: 92 ms (76.11%), Space: 49.2 MB (94.27%) - LeetHub

--- 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.js
+++ 2358-maximum-number-of-groups-entering-a-competition/2358-maximum-number-of-groups-entering-a-competition.js
@@ -1,5 +1,8 @@
 var maximumGroups = function(grades) {
     let count=1, length=grades.length;
-    while(length-count > count) length-=count++;
+    while(length-count > count) {
+        length-=count
+        count++
+    }
     return count;
 };
