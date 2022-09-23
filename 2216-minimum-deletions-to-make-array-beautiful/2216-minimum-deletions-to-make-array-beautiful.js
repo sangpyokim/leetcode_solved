abf72e28a1840839bad9626f8cdd1bafcf996fb7
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-23 20:04:52 +0900

    Time: 162 ms (46.94%), Space: 52.5 MB (73.47%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.js
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.js
@@ -3,15 +3,11 @@ var minDeletion = function (nums) {
 
     let deletes = 0;
     for (let i = 0; i < n - 1; i++) {
-        let j = i - deletes; // new i
-        if (
-            j % 2 === 0 && //
-            nums[i] === nums[i + 1]
-        ) {
+        let j = i - deletes;
+        if (j % 2 === 0 && nums[i] === nums[i + 1]) {
             deletes++;
         }
     }
 
-    // nums.length must be is even, if odd delete 1 more
     return deletes + ((n - deletes) % 2);
 };
