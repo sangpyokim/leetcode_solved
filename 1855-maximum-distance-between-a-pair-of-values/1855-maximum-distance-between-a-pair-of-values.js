ba09371c356d7f6927829980b6ce05fa0c9cffb3
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-23 21:10:57 +0900

    Time: 181 ms (23.13%), Space: 54.6 MB (38.78%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.js
@@ -3,11 +3,17 @@
  * @param {number[]} nums2
  * @return {number}
  */
+
+// 정렬되어있으면 투포인터 굳~
+// nums1[i] 보다 nums2가 작다면 계속 작은거임
 var maxDistance = function(nums1, nums2) {
     let answer = 0, i = 0, j = 0
     while(i < nums1.length && j < nums2.length) {
         if (nums1[i] > nums2[j]) i++
-        else answer = Math.max(answer, j++ - i)
+        else {
+            answer = Math.max(answer, j - i)
+            j++
+        }
     }
     return answer
 };
