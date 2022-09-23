2a2e9d1aeeb6ad7d1e5475374b3af6132c45e528
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-24 02:49:06 +0900

    Time: 69 ms (90.31%), Space: 42 MB (57.19%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -3,18 +3,7 @@
  * @return {number}
  */
 var countVowelStrings = function(n) {
-    
-    const dp = Array.from({length:n+1}, () => new Array(5).fill(0))
-    dp[1] = new Array(5).fill(1)
-    
-    for (let i = 2; i <= n; i++) {
-        dp[i][0] = dp[i-1].reduce((a,b) => a+b)
-        for (let j = 1; j < 5; j++) {
-            dp[i][j] = dp[i][j-1] - dp[i-1][j-1]
-        }
-    }
-    const len = dp.length-1
-    return dp[len].reduce((a,b) => a+b)
+    return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24;
 };
 // a e i o u
 // 1 1 1 1 1
