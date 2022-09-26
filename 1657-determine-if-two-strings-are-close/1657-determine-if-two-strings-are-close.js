085700fc667e5ab8b91dc8c0b9c2d156dc4629e7
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-27 02:04:40 +0900

    Time: 264 ms (51.52%), Space: 53.5 MB (21.21%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -1,39 +1,12 @@
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {boolean}
- */
-var closeStrings = function(word1, word2) {
-    if (word1.length !== word2.length) return false
-    
-    const arr1 = {}
-    const arr2 = {}
-    for (let i in word1) {
-        const n = word1[i]
-        const m = word2[i]
-        if (arr1[n]) arr1[n]++
-        else arr1[n] = 1
-        
-        if (arr2[m]) arr2[m]++
-        else arr2[m] = 1
-    }
+var closeStrings = function (w1, w2) {
+    const aCharCode = "a".charCodeAt();
+    // get counts of w1 & w2
+    counts1 = w1.split("").reduce((arr, c) => (arr[c.charCodeAt() - aCharCode]++, arr), new Array(26).fill(0));
+    counts2 = w2.split("").reduce((arr, c) => (arr[c.charCodeAt() - aCharCode]++, arr), new Array(26).fill(0));
 
-    const t1 = []
-    const t2 = []
-    for (let key in arr1) {
-        t1.push(arr1[key])
-    }
-    for (let key in arr2) {
-        t2.push(arr2[key])
-    }
-    for (let key in arr1) {
-        if (!arr2[key]) return false
-    }
-    t1.sort((a,b) => a-b)
-    t2.sort((a,b) => a-b)
-    if (t1.length !== t2.length) return false
-    for (let i in t1) {
-        if (t1[i] !== t2[i]) return false
-    }
-    return true
+    // same char, does not exist in both w1 & w2 - fail
+    if (counts1.some((_, i) => (counts1[i] && !counts2[i]) || (!counts1[i] && counts2[i]))) return false;
+
+    // just the counts, in order, of w1 & w2 - do they match?
+    return counts1.sort((a, b) => a - b).toString() === counts2.sort((a, b) => a - b).toString();
 };
