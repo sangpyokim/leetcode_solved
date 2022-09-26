cbaeafc574ef3d0c88b36043877c5e37f3143d7a
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-26 16:18:39 +0900

    Time: 391 ms (21.88%), Space: 65.2 MB (46.88%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.js
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.js
@@ -7,10 +7,12 @@
 var eliminateMaximum = function(dist, speed) {
     for (let i = 0; i < dist.length; i++) {
         dist[i] = (dist[i] - 1) / speed[i]
-    }
+    }    
     dist.sort((a,b) => a-b)
+    
     for (let i = 0; i < dist.length; i++) {
         if (i > dist[i]) return i
     }
+    
     return dist.length
 };
