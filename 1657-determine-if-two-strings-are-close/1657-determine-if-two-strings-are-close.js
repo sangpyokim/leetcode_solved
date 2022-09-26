9762bf1284fb782d38fa0fe93468490ca11d56e7
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-27 02:06:26 +0900

    Time: 177 ms (90.91%), Space: 49.4 MB (81.82%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.js
@@ -1,12 +1,16 @@
 var closeStrings = function (w1, w2) {
     const aCharCode = "a".charCodeAt();
-    // get counts of w1 & w2
-    counts1 = w1.split("").reduce((arr, c) => (arr[c.charCodeAt() - aCharCode]++, arr), new Array(26).fill(0));
-    counts2 = w2.split("").reduce((arr, c) => (arr[c.charCodeAt() - aCharCode]++, arr), new Array(26).fill(0));
 
-    // same char, does not exist in both w1 & w2 - fail
-    if (counts1.some((_, i) => (counts1[i] && !counts2[i]) || (!counts1[i] && counts2[i]))) return false;
+    let counts1 = new Array(26).fill(0); // char vs its counts, for every char, of w1
+    let counts2 = new Array(26).fill(0);
+    for (let i = 0; i < w1.length; i++) counts1[w1[i].charCodeAt() - aCharCode]++;
+    for (let i = 0; i < w2.length; i++) counts2[w2[i].charCodeAt() - aCharCode]++;
 
-    // just the counts, in order, of w1 & w2 - do they match?
+    for (let i = 0; i < 26; i++)
+        if ((counts1[i] && !counts2[i]) || (!counts1[i] && counts2[i])) {
+            return false; // same char, does not exist in both
+        }
+
+    // just counts in order, of w1 & w2 - do they match?
     return counts1.sort((a, b) => a - b).toString() === counts2.sort((a, b) => a - b).toString();
 };
