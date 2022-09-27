f4baf113702ee299b82fbc6a9b286d22829626ba
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-09-27 16:35:36 +0900

    Time: 185 ms (25.49%), Space: 46.1 MB (50.98%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.js
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.js
@@ -1,27 +1,21 @@
 var canConstruct = function(s, k) {
-	// if string length is less than k, we cannot form k palindromes from the string, so return false
     if(k > s.length) {
         return false;
     }
-	// created a map to keep count of each letter in the string
+
     const map = new Map();    // O(26)
-    for(let i = 0; i < s.length; ++i) {    // O(n)
-        map.set(s[i], map.has(s[i]) ? map.get(s[i]) + 1 : 1);
+    for(let char of s) {    // O(n)
+        map.set(char, map.get(char) + 1 || 1);
     }
-	// check how many letters have odd count
+    
     let oddCount = 0;
-    map.forEach((value, key) => {    // O(26)
-        if(value % 2 !== 0) {
-            ++oddCount;
-        }
-    })
-	// if letters with odd count are greater than k, that means more than k palindromes need to be formed to
-	// make palindromes from the string where all the letters are utilised, so return false
-	// palindrome can be of 2 form: `aaaaa` and `aabaa`, one letter can have odd and even occurences, but 
-	// there will be at least x palindromes if x letters are there with odd number of frequency
-    if(oddCount > k) {
-        return false;
+    
+    for (let [k, v] of map) {
+        if (v % 2 !== 0) oddCount++
+        
+        
     }
-	// since the previous conditions do not hold, we can make k palindromes from the string, so return true
+    if (oddCount > k) return false
+    
     return true;
 };
