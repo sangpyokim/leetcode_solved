001f5bdd10720a13cbbc1e470b7b00572c23e683
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-02 22:23:45 +0900

    Time: 78 ms (100.00%), Space: 42.3 MB (96.15%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.js
@@ -0,0 +1,22 @@
+var numRollsToTarget = function(d, f, target) {
+    const MOD = 1e9 + 7;
+    const dp = new Array(target + 1).fill(0);
+    
+    for (let face = 1; face <= Math.min(f, target); face++) {
+        dp[face] = 1;
+    }
+    
+    for (let dice = 2; dice <= d; dice++) {
+         for (let num = target; num > 0; num--) {
+            
+             dp[num] = 0;
+            
+             for (let face = 1; face <= Math.min(f, num); face++) {
+                const diff = num - face;
+                dp[num] = (dp[num] + dp[diff]) % MOD;
+            }
+        }
+    }
+    
+    return dp[target];
+};
