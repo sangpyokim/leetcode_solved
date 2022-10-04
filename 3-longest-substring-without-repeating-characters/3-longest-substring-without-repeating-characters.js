6fb4dab6cf622be3f337e94cb7b4ca1978a438b4
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-04 20:44:29 +0900

    Time: 119 ms (80.94%), Space: 44.2 MB (98.73%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -1,23 +1,15 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var lengthOfLongestSubstring = function(s) {
-	if (s.length < 2) return s.length;
-	let left = 0;
-	let right = 0;
-	let maxLength = 0;
-	let map = new Map()
-
-	while (right < s.length) {
-		var char = s.charAt(right);
-		
-		if (map.has(char)) {
-			maxLength = Math.max(maxLength, right - left);
-			left = Math.max(left, map.get(char) + 1);
-		}
-		
-		map.set(char, right++);
-	}
-	return Math.max(maxLength, right - left);
-};
+    const map = new Map()
+    let left = 0, right = 0;
+    let maxLen = 0
+    
+    for (right; right < s.length; right++) {
+        const cur = s[right]
+        
+        if (map.has(cur)) left = Math.max(left, map.get(cur) + 1)
+        map.set(cur, right)
+        maxLen = Math.max(maxLen, right - left + 1)
+    }
+    
+    return maxLen
+};  
