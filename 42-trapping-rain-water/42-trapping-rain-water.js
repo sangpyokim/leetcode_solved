d2c2761291fd8597c202c57c6dd2f168d6d9c649
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-08 19:24:12 +0900

    Time: 103 ms (65.10%), Space: 42.9 MB (74.32%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.js
+++ 42-trapping-rain-water/42-trapping-rain-water.js
@@ -1,22 +1,24 @@
 function trap(height) {
-  if (height == null || height.length === 0) return 0;
-
-  let l = 0;
-  let r = height.length - 1;
-
-  let lMax = 0;
-  let rMax = 0;
-
-  let res = 0;
-
-  while (l < r) {
-    lMax = Math.max(lMax, height[l]);
-    rMax = Math.max(rMax, height[r]);
-    res += lMax - height[l];
-    res += rMax - height[r];
-
-    height[l] < height[r] ? l++ : r--;
-  }
-
-  return res;
+    if (height == null || height.length === 0) return 0;
+    
+    let left = 0, right = height.length-1
+    let lMax = 0, rMax = 0
+    let answer = 0
+    
+    
+    while(left < right) {
+        const l = height[left]
+        const r = height[right]
+        
+        lMax = Math.max(lMax, l)
+        if (lMax > l) answer += lMax - l
+        
+        rMax = Math.max(rMax, r)
+        if (rMax > r) answer += rMax - r
+        
+        l < r ? left++ : right--
+    }
+    
+    
+    return answer
 }
