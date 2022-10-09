8653e40c32a432a4278fc343348855574d8411dc
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-09 18:18:26 +0900

    Time: 164 ms (35.90%), Space: 49.2 MB (63.53%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -3,8 +3,10 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    const len = s.length
-    for (let i = 0; i < Math.floor(len / 2); i++) {
-        [s[i], s[len-1-i]] = [s[len-1-i], s[i]]
+    let left = 0, right = s.length-1
+    while(left < right) {
+        [s[left], s[right]] = [s[right], s[left]]
+        left++
+        right--
     }
 };
