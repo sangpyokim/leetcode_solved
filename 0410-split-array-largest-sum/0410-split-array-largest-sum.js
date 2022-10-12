50e3effa33c6d80c047351c2bc2cb1e0c2a45093
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-12 18:03:34 +0900

    Time: 81 ms (77.53%), Space: 44.8 MB (7.49%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var splitArray = function(nums, k) {
+    let left = 0, right = 0
+    for (let num of nums) {
+        left = Math.max(num, left)
+        right += num
+    }
+
+    while(left < right) {
+        let mid = Math.floor((left + right) / 2)
+        let total = 0, count = 1
+        for (let num of nums) {
+            if (total + num <= mid) {
+                total += num
+            } else {
+                total = num
+                count+=1
+            }
+        }
+        if (count > k) left = mid+1
+        else right = mid
+    }
+    return left
+};
