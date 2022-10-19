41f43b1a660c7827ee59f6d37a205a4b2f5e20f3
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-19 19:25:49 +0900

    Time: 228 ms (65.57%), Space: 52.7 MB (54.15%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -11,6 +11,11 @@ var threeSum = function(nums) {
     let left = 0, right = len-1, mid = left+1
     
     while(left < len-2) {
+        if (nums[left] > 0) break
+        if (left > 0 && nums[left] === nums[left - 1]) {
+            left++
+            continue
+        }
         right = len-1
         mid = left+1
         
@@ -18,15 +23,13 @@ var threeSum = function(nums) {
             let sum = nums[left] + nums[mid] + nums[right]
             
             if (sum === 0) {
-                const str = `${nums[left]}${nums[mid]}${nums[right]}`
+
+                answer.push([nums[left], nums[mid], nums[right]])
+                while(nums[mid] === nums[mid+1]) mid++
+                while(nums[right] === nums[right-1]) right--
                 
-                
-                if (!set.has(str)) {
-                    set.add(str)
-                    answer.push([nums[left], nums[mid], nums[right]])
-                }
-                right--
                 mid++
+                right--
                 // break;
             } else if (sum > 0) {
                 right--
@@ -40,3 +43,47 @@ var threeSum = function(nums) {
     
     return answer
 };
+
+// {
+//     const results = []
+
+// 	if (nums.length < 3) return results
+
+// 	nums = nums.sort((a, b) => a - b)
+
+// 	let target = 0
+
+// 	for (let i = 0; i < nums.length - 2; i++) {
+// 		if (nums[i] > target) break
+
+// 		if (i > 0 && nums[i] === nums[i - 1]) continue
+
+// 		let j = i + 1
+
+// 		let k = nums.length - 1
+		
+
+// 		while (j < k) {
+// 			let sum = nums[i] + nums[j] + nums[k]
+
+// 			if (sum === target) {
+// 				// store the valid threesum
+// 				results.push([nums[i], nums[j], nums[k]])
+
+// 				while (nums[j] === nums[j + 1]) j++
+// 				while (nums[k] === nums[k - 1]) k--
+
+// 				j++
+// 				k--
+
+// 			} else if (sum < target) {
+// 				j++
+
+// 			} else { // (sum > target)
+// 				k--
+// 			}
+// 		}
+// 	}
+
+// 	return results
+// }
