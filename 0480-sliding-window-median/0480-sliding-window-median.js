a9bb41e8452ea73509860928ee8b404b01324848
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-22 16:08:10 +0900

    Time: 118 ms (92.71%), Space: 47.5 MB (52.08%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.js
+++ 0480-sliding-window-median/0480-sliding-window-median.js
@@ -5,56 +5,51 @@
  */
 const medianSlidingWindow = (nums, k) => {
 	const arr = []
-	const output = []
+	const res = []
 	const isEven = k % 2 === 0
 	const m = k >> 1
 
 	for (let i = 0; i < nums.length; i++) {
-		binaryInsertion(arr, nums[i])
+		binaryInsert(arr, nums[i])
 
 		if (arr.length > k) {
-			binaryDeletion(arr, nums[i - k])
+			binaryDelete(arr, nums[i - k])
 		}
 
 		if (arr.length === k) {
-			output.push(isEven ? (arr[m - 1] + arr[m]) / 2 : arr[m])
+			res.push(isEven ? (arr[m - 1] + arr[m]) / 2 : arr[m])
 		}
 	}
-
-	return output
+	return res
 }
-
-const binaryInsertion = (arr, target) => {
-	let left = 0
-	let right = arr.length
-
-	while (left < right) {
-		const mid = (left + right) >> 1
-
-		if (target > arr[mid]) {
-			left = mid + 1
-		} else {
-			right = mid
-		}
-	}
-
-	arr.splice(left, 0, target)
+function binaryInsert(arr, target) {
+    let left = 0, right = arr.length
+    
+    while(left < right) {
+        let mid = (right + left) >> 1
+        
+        if (arr[mid] < target) {
+            left = mid +1
+            
+        } else {
+            right = mid
+        }
+    }
+    
+    arr.splice(left, 0 , target)
 }
-
-const binaryDeletion = (arr, target) => {
-	let left = 0
-	let right = arr.length
-
-	while (left < right) {
-		const mid = (left + right) >> 1
-
-		if (target === arr[mid]) {
-			arr.splice(mid, 1)
-			break
-		} else if (target > arr[mid]) {
-			left = mid + 1
-		} else {
-			right = mid
-		}
-	}
+function binaryDelete(arr, target) {
+    let left = 0, right = arr.length
+    while(left < right) {
+        const mid = (right + left) >> 1
+        
+        if (arr[mid] === target) {
+            arr.splice(mid, 1)  
+            break
+        } else if (arr[mid] < target) {
+            left = mid+1
+        } else {
+            right = mid
+        }
+    }
 }
