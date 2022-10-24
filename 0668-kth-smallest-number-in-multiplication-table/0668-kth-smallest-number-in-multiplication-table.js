e658ce6e2dab1e5e66e522f4449a04a08519cb7e
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-24 22:51:05 +0900

    Time: 93 ms (79.17%), Space: 41.1 MB (100.00%) - LeetHub

--- 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.js
+++ 0668-kth-smallest-number-in-multiplication-table/0668-kth-smallest-number-in-multiplication-table.js
@@ -5,21 +5,27 @@
  * @return {number}
  */
 var findKthNumber = function(m, n, k) {
+    // left = 구구단은 1단부터, right = 구구단의 최대 수
     let left = 1, right = m*n
     
     while(left < right) {
         const mid = (right + left) >> 1
-        if (helper(mid) >= k) right = mid
-        else left = mid +1
+        if (helper(mid)) right = mid 
+        else left = mid+1
     }
-    
     function helper(mid) {
+        let res = false
         let count = 0
         for (let i = 1; i <= m; i++) {
-            count += Math.min(Math.floor(mid / i), n)
+            count += Math.min(Math.floor(mid/i), n)
+            if (count >= k) return true
         }
-        return count
+        return res
     }
-    
     return left
 };
+// 여기에서 n의 의미는 한줄의 개수 === mid/i해도 n개를 넘지 못함.
+// 5 1 => 5개, 3개
+// 5 2 => 2개, 3개 
+// 5 3 => 1개, 3개
+// 5보다 작은 수의 개수는 3 2 1 => 6개 > k이기때문에 right의 값을 줄여서 더 작은값을 구함.
