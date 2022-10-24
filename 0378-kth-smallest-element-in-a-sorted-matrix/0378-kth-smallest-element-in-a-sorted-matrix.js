d74c9b07466d7644f832d71ee8fc71b287369c3f
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-24 16:55:53 +0900

    Time: 122 ms (66.52%), Space: 45.5 MB (71.40%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.js
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.js
@@ -4,10 +4,23 @@
  * @return {number}
  */
 var kthSmallest = function(matrix, k) {
-    matrix = matrix.flat()
-    matrix.sort((a,b) => a-b)
+    const n = matrix.length
+    let left = matrix[0][0], right = matrix[n-1][n-1]
     
-    let left = 0, right = matrix.length-1
+    while(left < right) {
+        const mid = (right + left) >> 1
+        // mid보다 작은 수 찾기.
+        let count = 0
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                const cur = matrix[i][j]
+                if (cur <= mid) count++
+                else break;
+            }
+        }
+        if (count < k) left = mid+1
+        else right = mid
+    }
     
-    return matrix[k-1]
+    return left
 };
