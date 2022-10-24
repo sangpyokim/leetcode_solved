eb78a4b9bb1342ff05bc9b5fdb0f3281bdd246e2
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-24 20:38:29 +0900

    Time: 232 ms (27.78%), Space: 53.2 MB (41.67%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.js
@@ -4,7 +4,6 @@
  * @return {number}
  */
 var minimumSize = function(nums, maxOperations) {
-    nums.sort((a,b) => a-b)
     let left = 1, right = Math.max(...nums)
     
     while(left < right) {
@@ -15,7 +14,6 @@ var minimumSize = function(nums, maxOperations) {
             // 9 - 1 > 8 / 5
             count += Math.floor((num-1) / mid)
         }
-        console.log(mid, count)
         if (count <= maxOperations) right = mid
         else left = mid +1
     }
