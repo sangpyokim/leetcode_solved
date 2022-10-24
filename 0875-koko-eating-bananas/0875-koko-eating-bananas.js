67d6cb3f5183617746fd497d78bc614df84ad501
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-24 11:30:03 +0900

    Time: 134 ms (44.75%), Space: 44.8 MB (87.26%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    // max, sum
+    let left = 1, right = 0
+    for (let pile of piles) {
+        right = Math.max(right, pile)
+    }
+    
+    
+    while(left < right) {
+        // 시간 당 먹는 개수
+        const mid = (right + left) >> 1
+        if (feasible(mid)) right = mid
+        else left = mid +1
+    }
+    
+    function feasible(mid) {
+        let hours = 0
+        for (let pile of piles) {
+            hours += Math.ceil(pile / mid)
+            if (hours > h) return false
+        }
+        
+        return true
+    }
+    
+    return left
+};
