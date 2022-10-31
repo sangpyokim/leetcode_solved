8ba277924a3ce37883911e7968ba795eaab32ce7
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-10-31 14:26:08 +0900

    Time: 92 ms (91.34%), Space: 52.2 MB (9.00%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -5,8 +5,7 @@
 var maxProfit = function(prices) {
     let min = Infinity, max = 0
     
-    for (let i in prices) {
-        const price = prices[i]
+    for (let price of prices) {
         min = Math.min(min, price)
         max = Math.max(max, price - min)
     }
