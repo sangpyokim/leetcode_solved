868f7a9a0167bf5a54e8f73bbedbe7470d0b73db
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-02 19:27:54 +0900

    Time: 84 ms (78.92%), Space: 43.7 MB (90.24%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.js
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -3,11 +3,30 @@ var isToeplitzMatrix = function(matrix) {
     const n = matrix[0].length;
     
     
-    for (let i = 1; i < m; i++) {
-        for (let j = 1; j < n; j++) {
-            if (matrix[i][j] != matrix[i - 1][j - 1]) return false;
-        }     
+    let answer = true
+
+    for (let i = 0; i < m; i++) {
+        // console.log("start", i, answer)
+        recursion(i, 0, matrix[i][0])
+        if (!answer) return false
     }
+    for (let i = 1; i < n; i++) {
+        // console.log("start", i, "!@#", answer)
+        recursion(0, i, matrix[0][i])
+        
+        if (!answer) return false
+    }
+    
+    function recursion(i, j, prev) {
+        if (i >= m || j >= n) return
+        
+        // console.log(i, j, prev, matrix[i][j], answer)
+        if (matrix[i][j] !== prev) return answer = false
+        
+        recursion(i+1, j+1, matrix[i][j])    
+    }
+    
+    
 	
-    return true;
+    return answer;
 };
