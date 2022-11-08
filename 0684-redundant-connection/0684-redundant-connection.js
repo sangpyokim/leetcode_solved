d9a5c4cc2491b12bead3dd57a1b362b166ee4c44
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-08 21:10:34 +0900

    Time: 146 ms (18.26%), Space: 46.8 MB (22.26%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.js
+++ 0684-redundant-connection/0684-redundant-connection.js
@@ -1,11 +1,38 @@
+/**
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
 var findRedundantConnection = function(edges) {
-    let par = new Array(edges.length+1).fill(0).map((v, i) => v = i)
-    const find = x => x === par[x] ? par[x] : par[x] = find(par[x])
-    const union = (x,y) => par[find(y)] = find(x)
+    // edge 마다 그래프에 추가 시키면서하기
+    // [[1,2],[1,3],[2,3]]
+    // x -> y 로 가는 경로가 있다면 return
+    // 1 -> 2 갈 수 없다면 추가
+    // 1 -> 3 마찬가지로 추가
+    // 2 -> 3 갈 수 있기때문에 return
+    const len = edges.length
+    const list = Array.from({ length: len+1 }, () => []) // 그래프
+
+    
+    function helper(from, to, set) {
+        if (set.has(from)) return
+        set.add(from)
+
+        if (from === to) return true
+        
+        let res = false
+        for (let next of list[from]) {
+            if (helper(next, to, set))  res = true
+        }
+        
+        return res
+    }  
     
     for (let [x, y] of edges) {
-        if(find(x) === find(y)) return [x, y];
-        else union(x, y)
+
+        if (helper(x, y, new Set())) return [x, y]
+        list[x].push(y)
+        list[y].push(x)
     }
+    
     
 };
