fbfef392bb6c96e19458fad59a96a10109b61fa8
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-11 11:24:23 +0900

    Time: 1532 ms (5.04%), Space: 47.7 MB (71.71%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
@@ -1,41 +1,41 @@
-const longestIncreasingPath = (matrix) => {
-    if(!matrix || matrix.length === 0) return 0;
+/**
+ * @param {number[][]} matrix
+ * @return {number}
+ */
+var longestIncreasingPath = function(matrix) {
+    let answer = 0
+    const row = matrix.length
+    const col = matrix[0].length
+    const dir = [[1,0],[0,1],[-1,0],[0,-1]]
+    const visited = Array.from({length: row}, () => new Array(col).fill(false))
+    const table = Array.from({length: row}, () => new Array(col).fill(1))
     
-    const memo = new Array(matrix.length)
-                        .fill(-1)
-                        .map(row => new Array(matrix[0].length).fill(-1));
-    
-    let max = 0;
-    for(let row = 0; row < matrix.length; row++) {
-        for(let col = 0; col < matrix[0].length; col++) {
-            max = Math.max(max, dfs(matrix, row, col, Number.MIN_SAFE_INTEGER, memo) );
-        }
-    }
-    
-    return max;
-};
-
-const dfs = (matrix, row, col, parent, memo) => {
-    if(
-        row < 0
-        || row >= matrix.length
-        || col < 0
-        || col >= matrix[0].length
-        || matrix[row][col] <= parent
-    ) return 0;
-    
-    if(memo[row][col] === -1) {
-        const   rowVector = [1, -1, 0, 0],
-                colVector = [0, 0, 1, -1];
-
-        let maxPath = 0;
-        for(let dir = 0; dir < 4; dir++) {
-			const maxForNode = 1 + dfs(matrix, row + rowVector[dir], col + colVector[dir], matrix[row][col], memo);
-            maxPath = Math.max(maxPath, maxForNode);
-        }
+    function helper(i, j, count) {
+        if (visited[i][j] || count < table[i][j]) return
+        answer = Math.max(answer, count)
+        visited[i][j] = true
         
-        memo[row][col] = maxPath;
+        table[i][j] = count
+
+        for (let [x, y] of dir) {
+            const dx = i + x
+            const dy = j + y
+            const cur = matrix[i][j]
+            if (dx >= 0 && dx < row && dy >= 0 && dy < col && cur < matrix[dx][dy]) {
+                helper(dx, dy, count+1)
+            }
+        }
+        visited[i][j] = false
+    }
+
+    
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            helper(i, j, 1)
+        }
     }
     
-    return  memo[row][col];
+    
+    return answer
+    
 };
