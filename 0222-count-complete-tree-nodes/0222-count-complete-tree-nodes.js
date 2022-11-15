325d31ca0fa74f4a53e6abe9daeccdd43601c469
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-15 21:24:43 +0900

    Time: 90 ms (97.72%), Space: 60.8 MB (99.47%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,21 @@
+var countNodes = function(root) {
+    
+    function leftDepth(node) {
+        if(!node) return 0;
+        return leftDepth(node.left) + 1;
+    }
+    
+    function rightDepth(node) {
+        if(!node) return 0;
+        return rightDepth(node.right) + 1;
+    }
+    
+    function traverse(node) {
+        const leftLen = leftDepth(node);
+        const rightLen = rightDepth(node);
+        
+        if(leftLen === rightLen) return Math.pow(2, leftLen) - 1;
+        return traverse(node.left) + traverse(node.right) + 1;
+    }
+    return traverse(root);
+};
