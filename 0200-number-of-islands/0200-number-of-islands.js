ef254e87f7f786330dad49d99e16edab39f775dd
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-21 19:12:10 +0900

    Time: 162 ms (32.39%), Space: 51 MB (27.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -8,33 +8,34 @@ var numIslands = function(grid) {
     const col = grid[0].length
     const dir = [[1,0],[0,1],[-1,0],[0,-1]]
     
-    
-    function helper(i, j) {
-        if (grid[i][j] === '0') return
-        grid[i][j] = '0'
-        
-        for (let [x, y] of dir) {
-            const dx = i + x
-            const dy = j + y
-            
-            if (dx >= 0 && dx < row && dy >= 0 && dy < col) {
-                helper(dx, dy)
-            }
-        }
-        
-        
-    }
-
-    
-    
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col; j++) {
             if (grid[i][j] === '1') {
-                helper(i, j)
+                bfs(i, j)                
                 res += 1
             }
         }
-    } 
+    }
+    
+    function bfs(i, j) {
+        let q = [[i, j]]
+        while(q.length) {
+           const temp = []
+           for (let [a, b] of q) {
+               if (grid[a][b] === '0') continue
+               grid[a][b] = '0'
+               
+               for (let [x, y] of dir) {
+                   const dx = a + x
+                   const dy = b + y
+                   if (dx >= 0 && dx < row && dy >= 0 && dy < col) {
+                       temp.push([dx, dy])
+                   }
+               }
+            }
+            q = temp        
+        }
+    }
     
     
     return res
