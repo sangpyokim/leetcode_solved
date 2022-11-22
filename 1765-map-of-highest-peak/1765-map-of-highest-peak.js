d8778215a30206b79be5c2bbfffacf62977f0765
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-22 20:29:41 +0900

    Time: 1312 ms (61.54%), Space: 142.2 MB (30.77%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[][]} isWater
+ * @return {number[][]}
+ */
+var highestPeak = function(isWater) {
+    const row = isWater.length
+    const col = isWater[0].length
+    
+    const dir = [[1,0],[0,1],[-1,0],[0,-1]]
+    const grid = isWater.map(() => new Array(col).fill(Infinity))
+    
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (isWater[i][j] === 1) grid[i][j] = 0
+        }
+    }
+    let q = []
+    for (let i = 0; i < row; i++) {
+        for (let j = 0; j < col; j++) {
+            if (grid[i][j] === 0) q.push([i, j])
+        }
+    }
+    
+
+    const visited = isWater.map(() => new Array(col).fill(false))
+    let height = 0
+
+
+    while(q.length) {
+        const temp = []
+        for (let [i, j] of q) {
+            grid[i][j] = Math.min(grid[i][j], height)
+
+            for (let [x, y] of dir) {
+                const dx = i + x
+                const dy = j + y
+                if (dx >= 0 && dx < row && dy >= 0 && dy < col && !visited[dx][dy]) {
+                    temp.push([dx, dy])
+                    visited[dx][dy] = true
+                }
+            }
+        } 
+        height += 1
+        q = temp
+    }
+            
+    
+    return grid
+};
