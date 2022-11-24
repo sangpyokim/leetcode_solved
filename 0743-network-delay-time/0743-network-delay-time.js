18b5eebbf9c7df3869c676d56528069d0f975c20
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-24 09:51:30 +0900

    Time: 223 ms (45.05%), Space: 50.7 MB (66.45%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var networkDelayTime = function(times, n, k) {
+    const list = Array.from({ length: n+1 }, () => new Array(n+1).fill(Infinity))  
+    
+    for (let [x, y, z] of times) {
+        list[x][y] = z
+    }
+    for (let i = 1; i <= n; i++) {
+        list[i][i] = 0
+    }
+    
+    for (let k = 1; k <= n; k++) {
+        for (let i = 1; i <= n; i++) {
+            for (let j = 1; j <= n; j++) {
+                list[i][j] = Math.min(list[i][j], list[i][k] + list[k][j])
+            }
+        }
+    }
+    console.log(list[k])
+    let res = 0
+    for (let i = 1; i <= n; i++) {
+        if (list[k][i] === Infinity) return -1
+        res = Math.max(res, list[k][i])
+    }
+
+    return res
+};
