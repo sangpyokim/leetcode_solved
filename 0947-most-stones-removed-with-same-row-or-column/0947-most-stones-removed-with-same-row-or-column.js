68cededc717160d30e99f382b766b276e405df42
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-26 15:54:45 +0900

    Time: 151 ms (80.67%), Space: 45.7 MB (76.47%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.js
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.js
@@ -1,48 +1,36 @@
+/**
+ * @param {number[][]} stones
+ * @return {number}
+ */
 var removeStones = function(stones) {
-
-    let subsetCount = stones.length;
-    const parentMap = [];
-    
-    // Initialize the parent map to give each stone it's own set
-    for (let i = 0; i < stones.length; i++) {
-        parentMap[i] = i;
+    const len = stones.length
+    const par = Array.from({ length: len }, (v, i) => i)
+    const find = (x) => x === par[x] ? x : par[x] = find(par[x])
+    const union = (x, y) => {
+        if (x > y) return union(y, x)
+        x = find(x)
+        y = find(y)
+        par[y] = x
     }
+    const dir = [[1,0],[0,1],[-1,0],[0,-1]]
     
-    for (let thisStonesIdx = 1; thisStonesIdx < stones.length; thisStonesIdx++) {           
-        const thisStone = stones[thisStonesIdx];
-        
-        for (let thatStonesIdx = 0; thatStonesIdx < thisStonesIdx; thatStonesIdx++) {
-            const thatStone = stones[thatStonesIdx];
+    for (let i = 0; i < len; i++) {
+        const [a, b] = stones[i]
             
-            // Not in the same row or column, skip ahead
-            if (thisStone[0] !== thatStone[0] && thisStone[1] !== thatStone[1]) continue;
-            
-            // If this stone isn't already part of an existing subset
-            if (parentMap[thisStonesIdx] === thisStonesIdx) {
-                // Add it to that one's subset
-                parentMap[thisStonesIdx] = thatStonesIdx;
-                subsetCount -= 1;
-            } else {
-                // Find this stone's root
-                let currentThisStonesParentIndex = parentMap[thisStonesIdx];
-                while (parentMap[currentThisStonesParentIndex] !== currentThisStonesParentIndex) {
-                    currentThisStonesParentIndex = parentMap[currentThisStonesParentIndex];
-                }
-                
-                // Find that stone's root
-                let currentThatStonesParentIndex = parentMap[thatStonesIdx];
-                while (parentMap[currentThatStonesParentIndex] !== currentThatStonesParentIndex) {
-                    currentThatStonesParentIndex = parentMap[currentThatStonesParentIndex];
-                }
-
-                // If they're not in the same subset, merge them
-                if (currentThisStonesParentIndex != currentThatStonesParentIndex) {
-                    parentMap[currentThisStonesParentIndex] = currentThatStonesParentIndex;  
-                    subsetCount -= 1;
-                }
-            }
+        for (let j = i+1; j < len; j++) {
+            const [c, d] = stones[j]
+            let check = false
+            if (check) continue
+            if (a === c || b === d) {
+                union(i, j)
+                check = true
+            }                
         }
     }
+    for (let i in par) {
+        par[i] = find(par[i])
+    }
+    const set = new Set(par)
     
-    return stones.length - subsetCount;
+    return len - set.size
 };
