0390323f70f394b59d43ec8ee29a5834bb25c89e
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-29 16:51:13 +0900

    Time: 159 ms (57.74%), Space: 47.3 MB (72.97%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    let dist = Array.from({ length: n}, () => Infinity)
+    dist[src] = 0
+    
+    for (let i = 0; i < k+1; i++) {
+        let temp = [...dist]
+        for (let [start, end, weight] of flights) {
+
+            if (dist[start] === Infinity) continue
+            if (temp[end] > dist[start] + weight) {
+                temp[end] = dist[start] + weight
+            }
+        }
+        dist = [...temp]
+    }
+    return dist[dst] === Infinity ? -1 : dist[dst]
+};
