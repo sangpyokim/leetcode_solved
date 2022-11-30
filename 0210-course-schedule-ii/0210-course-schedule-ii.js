6752d7dcb0ddc7f5340fa6534783389cfbada17e
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-30 16:04:37 +0900

    Time: 80 ms (96.48%), Space: 46.7 MB (82.56%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -1,44 +1,29 @@
-let visited;
-let graph;
-let stack;
-
 var findOrder = function(numCourses, prerequisites) {
     
-    graph = new Map();
-    visited = new Array(numCourses).fill(0);
-    stack = new Array();
+    const list = Array.from({ length: numCourses }, () => [])
+    const visited = Array.from({ length: numCourses }, () => 0)
+    const stack = []
     
-    for(let [v, e] of prerequisites){
-        if(graph.has(v)){
-            let values = graph.get(v);
-            values.push(e);
-            graph.set(v, values)
-        } else {
-            graph.set(v, [e])
+    for (let [x, y] of prerequisites) {
+        list[x].push(y)
+    }
+    
+    for (let i = 0; i < numCourses; i++) {
+        if (visited[i] === 0 && dfs(i)) return []
+    }
+    
+    function dfs(node) {
+        visited[node] = 1
+        
+        for (let x of list[node]) {
+            if (visited[x] === 1) return true
+            if (visited[x] === 0 && dfs(x)) return true
         }
+        
+        visited[node] = 2
+        stack.push(node)
+        return false
     }
-    
-    for(let i = 0; i < numCourses; i++){
-        if(visited[i] == 0 && DFS(i)) return [];
-    }
-    
-    return stack;
-}
 
-
-function DFS(index){
-    
-    visited[index] = 1;
-    let edges = graph.get(index);
-    
-    if(edges){
-        for(let e of edges){
-            if(visited[e] == 1) return true;
-            if(visited[e] == 0 && DFS(e)) return true
-        }  
-    }
-  
-    visited[index] = 2;
-    stack.push(index)
-    return false
-}
+    return stack
+};
