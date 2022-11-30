389eb804b43f180a9a12ea42f65c240d1b774fb4
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-11-30 17:47:11 +0900

    Time: 182 ms (22.52%), Space: 48.6 MB (35.54%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -1,29 +1,36 @@
 var findOrder = function(numCourses, prerequisites) {
     
     const list = Array.from({ length: numCourses }, () => [])
-    const visited = Array.from({ length: numCourses }, () => 0)
-    const stack = []
-    
+    const indegree = Array.from({ length: numCourses }, () => 0)
+    const q = []
+    const topoSort = []
     for (let [x, y] of prerequisites) {
         list[y].push(x)
+        indegree[x] += 1
     }
     
-    for (let i = 0; i < numCourses; i++) {
-        if (visited[i] === 0 && dfs(i)) return []
-    }
-    
-    function dfs(node) {
-        visited[node] = 1
-        
-        for (let x of list[node]) {
-            if (visited[x] === 1) return true
-            if (visited[x] === 0 && dfs(x)) return true
+    for (let i in indegree) {
+        const x = indegree[i]
+        if (x === 0) {
+            q.push(i*1)
         }
+    }
+    
+    
+    
+    let prereq = 0
+    while(q.length > 0) {
+        const cur = q.shift()
+        prereq += 1
+        if (indegree[cur] === 0) topoSort.push(cur)
         
-        visited[node] = 2
-        stack.push(node)
-        return false
+        const nextNodes = list[cur]
+        for (let nextNode of nextNodes) {
+            if (--indegree[nextNode] === 0) {
+                q.push(nextNode)
+            }
+        }
     }
 
-    return stack.reverse()
+    return prereq === numCourses ? topoSort : []
 };
