3df467e4a0938561797c43d1bb8b3329129ad258
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-02 13:25:09 +0900

    Time: 3096 ms (15.79%), Space: 131.6 MB (55.62%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.js
@@ -1,73 +1,39 @@
-class DSU {
-    ranks;
-    parents;
-    
-    constructor(length) {
-        this.ranks = new Array(length).fill(1);
-        this.parents = Array.from({length}, (_, idx) => idx);
-    }
-    
-    find(x) {
-        if (x !== this.parents[x]) {
-            this.parents[x] = this.find(this.parents[x]);
-        }
-        return this.parents[x];
-    }
-    
-    union(x, y) {
-        x = this.find(x);
-        y = this.find(y);
-        
-        if (x === y) {
-            return false;
-        }
-        if (this.ranks[x] < this.ranks[y]) {
-            [x, y] = [y, x];
-        }
-        this.ranks[x] += this.ranks[y];
-        this.parents[y] = x;
-        
-        return true;
-    }
-}
-
 /**
  * @param {number[][]} points
  * @return {number}
  */
 function minCostConnectPoints(points) {
-    const connections = uniqueConnections(points);
-    connections.sort((a, b) => a[2] - b[2]);
-    const par = Array.from({ length: points.length }, (_, i) => i)
+    const len = points.length
+    const par = Array.from({ length: len }, (_, i) => i)
     const find = x => x === par[x] ? x : par[x] = find(par[x])
     const union = (x, y) => {
         if (x > y) return union(y, x)
         x = find(x)
         y = find(y)
-        
         par[x] = y
     }
-    
-    let totalCost = 0;
-    for (const [a, b, cost] of connections) {
+    let res = 0
+    const edges = makeEdges(points, len)
+    edges.sort((a,b) => a[2] - b[2])
+    for (let [a,b, w] of edges) {
         if (find(a) !== find(b)) {
-            union(a, b)
-            totalCost += cost;
-        }
+            union(a,b)
+            res += w
+        }        
     }
-    return totalCost;
-}
-
-function uniqueConnections(arr) {
-    let res = []
-    for (let i = 0; i < arr.length - 1; i++) {
-        for (let j = i + 1; j < arr.length; j++) {
-            res.push([i, j, dist(arr[i], arr[j])])
-        }
-    }
+    
     return res
 }
 
-function dist([x1, y1], [x2, y2]) {
-    return Math.abs(x1 - x2) + Math.abs(y1 - y2);
-}
+const makeEdges = (arr, n) => {
+    const res = []
+    for (let i = 0; i < n; i++) {
+        for (let j = i+1; j < n; j++) {
+            const [a,b] = arr[i]
+            const [c,d] = arr[j]
+            const temp = Math.abs(a - c) + Math.abs(b - d)
+            res.push([i, j, temp])
+        }
+    }
+    return res
+} 
