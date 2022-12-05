1bd4771604dea7ba4bfb7466c209fbf66dd20e86
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-05 09:58:19 +0900

    Time: 262 ms (5.03%), Space: 41.6 MB (76.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -7,13 +7,9 @@ function fib(n) {
     
     function dfs(num) {
         if (num < 2) return num;
-        
-        let res = 0
-        if (memo.has(num-1)) res += memo.get(num-1)
-        else memo.set(num-1, dfs(num-1))
-        if (memo.has(num-2)) res += memo.get(num-2)
-        else memo.set(num-2, dfs(num-2))
 
+        memo.set(num-1, dfs(num-1))
+        memo.set(num-2, dfs(num-2))
         
         return memo.get(num-1) + memo.get(num-2)
     }
