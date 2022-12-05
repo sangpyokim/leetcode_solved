8f163bd3406b7332b2cb34ceb064c99475dcfd30
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2022-12-05 20:48:29 +0900

    Time: 87 ms (75.70%), Space: 42.2 MB (14.08%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -5,28 +5,20 @@
 
 // 하향식
 function fib(n) {
-    const memo = new Map()
-    
-    function helper(num) {
-        if (num < 2) return num
-        if (memo.has(num)) return memo.get(num)
-        
-        memo.set(num, helper(num-1) + helper(num-2))
-        
-        return memo.get(num)
-    }
-    
-    return helper(n)
-};
-
-
-// 상향식
-function fib(n) {
     const dp = []
     dp[0] = 0
     dp[1] = 1
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i-1] + dp[i-2]
-    }
+    
+    function helper(L) {
+        if (L > n) return 
+        
+        dp[L] = dp[L-1] + dp[L-2]
+        
+        return helper(L+1)
+    } 
+    
+    helper(2)
+    
+    
     return dp[n]
 };
