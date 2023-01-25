101fa5fcacbd81367494d9b82e53a7a6e700f810
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-01-26 04:06:14 +0900

    Time: 406 ms (50.00%), Space: 65.5 MB (83.33%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.js
@@ -0,0 +1,30 @@
+function closestMeetingNode (edges, node1, node2) {
+    let map1 = {}
+    let map2 = {}
+    let count = 0;
+
+    while(map1[node1] == undefined && node1 != -1){
+        map1[node1] = count;
+        count++
+        node1 = edges[node1];
+    }
+    count = 0;
+    while(map2[node2] == undefined && node2 != -1){
+        map2[node2] = count;
+        count++
+        node2 = edges[node2]
+    }
+    let max = Infinity;
+    let res = -1;
+
+    for(let i =0; i<edges.length;i++){
+        if(map1[i] == undefined || map2[i] == undefined) continue;
+        let localMax = Math.max(map1[i],map2[i])
+        if(localMax<max){
+            max = localMax;
+            res = i;
+        }
+    }
+
+    return res;
+}
