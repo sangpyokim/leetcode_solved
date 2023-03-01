618e02efaa1ad266b6691bc6e30761fc564f8faa
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-03-01 11:36:33 +0900

    Time: 1211 ms (31.80%), Space: 65.9 MB (29.14%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,23 @@
+const sortArray = nums => {
+    if(nums.length <= 1) return nums
+    
+    const middle = Math.floor(nums.length / 2)
+    const left = nums.slice(0, middle)
+    const right = nums.slice(middle)
+    
+    return merge(sortArray(left), sortArray(right))
+};
+
+const merge = (left, right) => {
+    const result = []
+    
+    while (left.length && right.length) {
+        if(left[0] <= right[0]) {
+            result.push(left.shift())
+        } else {
+            result.push(right.shift())
+        }
+    }
+    
+    return [...result, ...left, ...right]
+}
