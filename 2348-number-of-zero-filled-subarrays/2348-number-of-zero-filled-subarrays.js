83fce25103a2179cd32eacc033f7d2f43b8e40df
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-03-21 16:04:16 +0900

    Time: 89 ms (78.95%), Space: 53.6 MB (55.26%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.js
@@ -0,0 +1,11 @@
+const zeroFilledSubarray = nums => {
+    let count = 0;
+    let length = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            length++;
+            count += length;
+        } else length = 0;
+    }
+    return count;
+};
