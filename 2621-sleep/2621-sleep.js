39f707bc67cc829b4f1f7434f452e6f2c36c31a5
sangpyokim <59750729+sangpyokim@users.noreply.github.com>
2023-05-19 17:42:54 +0900

    Time: 64 ms (31.34%), Space: 42.1 MB (13.87%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    
+    return new Promise((res, req) => {
+        setTimeout(res, millis)
+    })
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
